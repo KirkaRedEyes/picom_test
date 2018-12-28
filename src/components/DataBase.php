bca05d6d1b6a87d0b1edd693cc0629f94d42ab93
Kirill Petrov <ttarazut@gmail.com>
2018-12-28 10:45:45 +0400

    Fixed

--- src/components/DataBase.php
+++ src/components/DataBase.php
@@ -7,18 +7,6 @@ class DataBase
     {
         $dbConfig = require './config/db.php';
         ORM::configure($dbConfig);
-
-        //todo Миграции
-        ORM::raw_execute('CREATE TABLE IF NOT EXISTS `user` (
-            `id` INT NOT NULL AUTO_INCREMENT ,
-            `first_name` VARCHAR(255) NOT NULL ,
-            `last_name` VARCHAR(255) NOT NULL ,
-            `phone` VARCHAR(255) NOT NULL ,
-            `email` VARCHAR(255) NOT NULL ,
-            `location` TEXT NOT NULL ,
-            `registered_at` INT NOT NULL ,
-            PRIMARY KEY (`id`)
-        ) ENGINE = InnoDB;');
     }
 
     public static function selectUsers()
